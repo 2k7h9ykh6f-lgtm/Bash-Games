5802f7f4d871b06778e859aa049d5fb902c8d5ee
2k7h9ykh6f-lgtm <345750680@qq.com>
2026-06-15 10:48:34 +0800

    feat: implement p-3 Claude changes

--- snake.sh
+++ snake.sh
@@ -93,7 +93,13 @@ snake_init() {
     ypt=(5 4 3 2 1);                          #开始的各个节点的y坐标
     speed=(0.05 0.1 0.15);  spk=${spk:-1};    #速度 默认速度
 
+    obs_num=$(((Lines+Cols)/8)); ((obs_num<5)) && obs_num=5;  #障碍数量
+    obs_x=(); obs_y=();                        #障碍坐标(行/列)
+    hflag=0; hx=0; hy=0; hscore=0; htimer=0;   #限时高分食物: 是否存在/坐标/分值/倒计时
+    sflag=0; sx=0; sy=0;                        #减速食物: 是否存在/坐标
+
     draw_gui $((Lines-1)) $Cols
+    gen_obstacles                             #在棋盘上生成障碍
 }
 
 game_pause() {                                #暂停游戏
@@ -153,16 +159,87 @@ add_node() {                                 #增加节点
         (( ${xpt[0]} == ${xpt[$i]} && ${ypt[0]} == ${ypt[$i]} )) && return 1; #crashed
     done
 
+    hit_obstacle ${xpt[0]} ${ypt[0]} && return 1; #撞障碍
+
     echo -ne "\033[${xpt[0]};${ypt[0]}H\033[32m${snake[@]:0:1}\033[0m";
     return 0;
 }
 
-mk_random() {                               #产生随机点和随机数
-    xrand=$((RANDOM%(Lines-3)+2));
-    yrand=$((RANDOM%(Cols-2)+2));
-    foodscore=$((RANDOM%9+1));
+# 颜色图例(与现有 UI 区分):
+#   边框 蓝 *   蛇 绿 0   普通食物 白色数字
+#   障碍 红 #   限时高分食物 亮品红 $   减速食物 亮青 %
+is_free() {                                 #坐标(x行 y列)是否空闲
+    local x=$1 y=$2 i
+    (( x<2 || x>Lines-2 || y<2 || y>Cols-1 )) && return 1   #在边框上/外
+    for (( i = 0; i < ${#xpt[@]}; i++ )); do                #蛇身
+        (( x==${xpt[$i]} && y==${ypt[$i]} )) && return 1
+    done
+    for (( i = 0; i < ${#obs_x[@]}; i++ )); do              #障碍
+        (( x==${obs_x[$i]} && y==${obs_y[$i]} )) && return 1
+    done
+    (( liveflag==0 && x==xrand && y==yrand )) && return 1   #普通食物
+    (( hflag==1 && x==hx && y==hy )) && return 1            #高分食物
+    (( sflag==1 && x==sx && y==sy )) && return 1            #减速食物
+    return 0
+}
 
-    echo -ne "\033[$xrand;${yrand}H$foodscore";
+rand_pos() {                                #取一个空闲随机点 -> rx ry
+    local try=0
+    while (( try < 200 )); do
+        rx=$((RANDOM%(Lines-3)+2));
+        ry=$((RANDOM%(Cols-2)+2));
+        is_free $rx $ry && return 0
+        ((try++))
+    done
+    return 1
+}
+
+gen_obstacles() {                           #生成不可穿越的障碍
+    obs_x=(); obs_y=();
+    local i
+    for (( i = 0; i < obs_num; i++ )); do
+        if rand_pos; then
+            obs_x+=($rx); obs_y+=($ry);
+            echo -ne "\033[$rx;${ry}H\033[1;31m#\033[0m";
+        fi
+    done
+}
+
+hit_obstacle() {                            #撞到障碍返回0(命中) 否则1
+    local x=$1 y=$2 i
+    for (( i = 0; i < ${#obs_x[@]}; i++ )); do
+        (( x==${obs_x[$i]} && y==${obs_y[$i]} )) && return 0
+    done
+    return 1
+}
+
+upd_special() {                             #限时高分食物倒计时 + 概率生成特殊食物
+    if (( hflag==1 )); then                 #高分食物到期则消失
+        ((htimer--));
+        (( htimer<=0 )) && { echo -ne "\033[$hx;${hy}H "; hflag=0; }
+    fi
+    if (( hflag==0 && RANDOM%100<3 )); then  #3% 生成限时高分食物
+        if rand_pos; then
+            hx=$rx; hy=$ry; hflag=1;
+            hscore=$((RANDOM%11+15));        #高分 15-25
+            htimer=$((RANDOM%20+30));        #存活 30-49 帧
+            echo -ne "\033[$hx;${hy}H\033[1;35m\$\033[0m";
+        fi
+    fi
+    if (( sflag==0 && RANDOM%100<2 )); then  #2% 生成减速食物
+        if rand_pos; then
+            sx=$rx; sy=$ry; sflag=1;
+            echo -ne "\033[$sx;${sy}H\033[1;36m%\033[0m";
+        fi
+    fi
+}
+
+mk_random() {                               #产生随机点和随机数(避开蛇身/边框/障碍/其它食物)
+    if rand_pos; then
+        xrand=$rx; yrand=$ry;
+        foodscore=$((RANDOM%9+1));
+        echo -ne "\033[$xrand;${yrand}H\033[37m$foodscore\033[0m";
+    fi
     liveflag=0;
 }
 
@@ -178,6 +255,7 @@ new_game() {                                #重新开始新游戏
         fi
 
         ((liveflag==0)) || mk_random;
+        upd_special;
         if (( sumnode > 0 )); then
             ((sumnode--));
             add_node; (($?==0)) || return 1;
@@ -196,8 +274,16 @@ new_game() {                                #重新开始新游戏
 
         local x=${xpt[0]} y=${ypt[0]}
         (( ((x>=$((Lines-1)))) || ((x<=1)) || ((y>=Cols)) || ((y<=1)) )) && return 1; #撞墙
+        hit_obstacle $x $y && return 1; #撞障碍
 
         (( x==xrand && y==yrand )) && ((liveflag=1)) && ((sumnode+=foodscore)) && ((sumscore+=foodscore));
+
+        if (( hflag==1 && x==hx && y==hy )); then     #吃到限时高分食物: 加高分
+            ((sumscore+=hscore)); ((sumnode+=3)); hflag=0; ch_speed 0;
+        fi
+        if (( sflag==1 && x==sx && y==sy )); then      #吃到减速食物: 降速并刷新速度提示
+            (( spk<2 )) && ((spk++)); ((sumscore+=2)); ((sumnode+=1)); sflag=0; ch_speed 0;
+        fi
 
         echo -ne "\033[$xscore;$((yscore-2))H$sumscore";
     done
